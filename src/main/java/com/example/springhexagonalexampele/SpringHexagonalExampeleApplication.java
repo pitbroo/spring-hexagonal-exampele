13608e64cb25dff7c83fd033cbcccbade1b7614c
Piotr <pitbroo@gmail.com>
2022-12-10 12:09:14 +0100

    Initial commit

---
+++ src/main/java/com/example/springhexagonalexampele/SpringHexagonalExampeleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springhexagonalexampele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalExampeleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHexagonalExampeleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springhexagonalexampele/SpringHexagonalExampeleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhexagonalexampele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalExampeleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
